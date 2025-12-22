040a8c8bf64e319d5ff01f49ae3808708268a30b
Aditya Motilal <adityamotilal.cs25@bmsce.ac.in>
2025-12-22 20:31:14 +0530

    fixer

--- spendwise_backend/spendwise_backend/settings.py
+++ spendwise_backend/spendwise_backend/settings.py
@@ -92,7 +92,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # --- REST FRAMEWORK ---
 REST_FRAMEWORK = {
