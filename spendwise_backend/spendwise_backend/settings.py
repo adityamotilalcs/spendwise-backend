d6c5d79fd690ee474afecabbcd41eb511c41d430
Aditya Motilal <adityamotilal.cs25@bmsce.ac.in>
2025-12-23 02:14:11 +0530

    fix

--- spendwise_backend/spendwise_backend/settings.py
+++ spendwise_backend/spendwise_backend/settings.py
@@ -25,8 +25,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'corsheaders',  # Correctly placed
+    # Your apps
+    'corsheaders',
     'rest_framework',
+    'rest_framework_simplejwt',  # <--- MAKE SURE THIS IS HERE
     'api',
 ]
 
