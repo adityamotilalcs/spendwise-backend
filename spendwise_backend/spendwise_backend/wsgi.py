e648f2d9a3d0c74bc39305f0830e599cc7a9154a
Aditya Motilal <adityamotilal.cs25@bmsce.ac.in>
2025-12-23 03:38:09 +0530

    fix

--- spendwise_backend/spendwise_backend/wsgi.py
+++ spendwise_backend/spendwise_backend/wsgi.py
@@ -1,10 +1,5 @@
 """
 WSGI config for spendwise_backend project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 """
 
 import os
@@ -14,3 +9,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spendwise_backend.settings')
 
 application = get_wsgi_application()
+
+# --- ADD THIS LINE BELOW ---
+app = application
