ad0c20c1a86bea4325e68a632683183ae993f2be
Aditya Motilal <adityamotilal.cs25@bmsce.ac.in>
2025-12-24 00:45:35 +0530

    fix

--- spendwise_backend/spendwise_backend/wsgi.py
+++ spendwise_backend/spendwise_backend/wsgi.py
@@ -3,12 +3,17 @@ WSGI config for spendwise_backend project.
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spendwise_backend.settings')
 
+# --- FIX FOR DJONGO + DRF TOKEN ERROR (Monkey Patch) ---
+# We apply this here to ensure it runs right before the app starts
+from rest_framework.authtoken.models import Token
+from django.db import models
+Token.objects = models.Manager()
+# -------------------------------------------------------
+
 application = get_wsgi_application()
 
-# --- ADD THIS LINE BELOW ---
 app = application
